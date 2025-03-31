fbdb955b6c43b3d614abe36e1e7586d2b30ab62e
Seona12 <vustjsdk1@naver.com>
2025-03-31 13:06:08 +0900

    UI 설계 및 사용자 입력 저장

--- AIcruiter/Form1.Designer.cs
+++ AIcruiter/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1_random = new System.Windows.Forms.Button();
+            this.btn2_load = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(207, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(385, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Al 면접관 시뮬레이터";
+            // 
+            // btn1_random
+            // 
+            this.btn1_random.Font = new System.Drawing.Font("나눔고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn1_random.Location = new System.Drawing.Point(101, 265);
+            this.btn1_random.Name = "btn1_random";
+            this.btn1_random.Size = new System.Drawing.Size(163, 90);
+            this.btn1_random.TabIndex = 1;
+            this.btn1_random.Text = "랜덤 질문";
+            this.btn1_random.UseVisualStyleBackColor = true;
+            this.btn1_random.Click += new System.EventHandler(this.btn1_random_Click);
+            // 
+            // btn2_load
+            // 
+            this.btn2_load.Font = new System.Drawing.Font("나눔고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn2_load.Location = new System.Drawing.Point(291, 265);
+            this.btn2_load.Name = "btn2_load";
+            this.btn2_load.Size = new System.Drawing.Size(183, 90);
+            this.btn2_load.TabIndex = 2;
+            this.btn2_load.Text = "내 답변 확인";
+            this.btn2_load.UseVisualStyleBackColor = true;
+            this.btn2_load.Click += new System.EventHandler(this.btn2_load_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("나눔고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(502, 265);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 90);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "키워드 확인";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn2_load);
+            this.Controls.Add(this.btn1_random);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1_random;
+        private System.Windows.Forms.Button btn2_load;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- AIcruiter/Form1.cs
+++ AIcruiter/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,82 @@ namespace AIcruiter
         {
             InitializeComponent();
         }
+        private void btn1_random_Click(object sender, EventArgs e)
+        {
+            // 질문 내용 (하나만 하드코딩으로 설정//추후 txt파일로 대체)
+            string question = "랜덤질문 생성";
+
+            // 모달창 생성
+            Form modalForm = new Form();
+            modalForm.Text = "면접 질문";
+            modalForm.Size = new System.Drawing.Size(400, 300);
+            modalForm.StartPosition = FormStartPosition.CenterParent;
+
+            // 질문을 보여줄 라벨
+            Label questionLabel = new Label();
+            questionLabel.Text = question;
+            questionLabel.AutoSize = true;
+            questionLabel.Location = new System.Drawing.Point(20, 20);
+            questionLabel.MaximumSize = new System.Drawing.Size(350, 0); // 줄바꿈 설정
+            modalForm.Controls.Add(questionLabel);
+
+            // 텍스트박스 (사용자 답변 입력)
+            TextBox answerBox = new TextBox();
+            answerBox.Multiline = true;
+            answerBox.Size = new System.Drawing.Size(350, 100);
+            answerBox.Location = new System.Drawing.Point(20, 80);
+            modalForm.Controls.Add(answerBox);
+
+            // 저장 버튼
+            Button saveButton = new Button();
+            saveButton.Text = "저장하기";
+            saveButton.Location = new System.Drawing.Point(20, 200);
+            saveButton.Click += (s, ev) =>
+            {
+                string userAnswer = answerBox.Text;
+
+                if (!string.IsNullOrWhiteSpace(userAnswer))
+                {
+                    try
+                    {
+                        // 질문과 답변을 함께 저장
+                        string content = $"질문: {question}\n답변: {userAnswer}\n\n";
+                        File.AppendAllText("user_input_one.txt", content); // 파일에 추가로 저장
+                        MessageBox.Show("질문과 답변이 저장되었습니다.", "저장 완료");
+                        modalForm.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("파일 저장 오류: " + ex.Message, "오류");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("답변을 입력해주세요.", "오류");
+                }
+            };
+            modalForm.Controls.Add(saveButton);
+
+            // 모달창 띄우기
+            modalForm.ShowDialog();
+        }
+
+        private void btn2_load_Click(object sender, EventArgs e)
+        {
+            // 파일 경로 지정
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "user_input_one.txt");
+
+            if (File.Exists(filePath))
+            {
+                string loadedText = File.ReadAllText(filePath);
+                MessageBox.Show(loadedText, "저장된 질문 및 답변");
+            }
+            else
+            {
+                MessageBox.Show("저장된 파일이 없습니다.", "오류");
+            }
+        }
+
+
     }
 }
