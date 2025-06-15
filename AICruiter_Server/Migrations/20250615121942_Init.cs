59122e11347c9a63856f82a36624856465529b8a
lsh8110 <lsklyj0118@naver.com>
2025-06-15 21:26:04 +0900

    마이그레이션 추가

---
+++ AICruiter_Server/Migrations/20250615121942_Init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AICruiter_Server.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SharedAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    QuestionId = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    Score = table.Column<int>(nullable: false),
+                    AnswerContent = table.Column<string>(nullable: true),
+                    SubmittedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedAnswers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SharedAnswers");
+        }
+    }
+}
